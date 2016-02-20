734a708ca518d0f92e428b588062b29382f61a2a
Leo Lacroix <lelacroi@asu.edu>
2016-02-19 21:26:28 -0700

    Initial commit of source code

---
+++ cse360assign3/src/cse360assign3/Calculator.java
@@ -0,0 +1,74 @@
+package cse360assign3;
+
+/**
+ * @author Leo
+ *
+ */
+public class Calculator {
+	
+	private int total;
+	
+	/**
+	 * 
+	 */
+	public Calculator () {
+		
+		total = 0;  // not needed - included for clarity
+	}
+	
+	
+	/**
+	 * @return
+	 */
+	
+	public int getTotal () {
+		
+		return 0;
+	}
+	
+	
+	/**
+	 * @param value
+	 */
+	public void add (int value) {
+		
+	}
+
+	
+	/**
+	 * @param value
+	 */
+	
+	public void subtract (int value) {
+		
+	}
+	
+	
+	/**
+	 * @param value
+	 */
+	
+	public void multiply (int value) {
+		
+	}
+	
+	
+	/**
+	 * @param value
+	 */
+	
+	public void divide (int value) {
+		
+	}
+	
+	
+	/**
+	 * @return
+	 */
+	
+	public String getHistory () {
+		
+		return "";
+	}
+
+}
